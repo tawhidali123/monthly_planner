64a17954accdac31b8c283e02d848e32ed71e846
Tawhid <gem@tawhid.local>
2019-10-28 13:52:57 -0400

    mvp made

--- FrontEnd/myScript.js
+++ FrontEnd/myScript.js
@@ -1,3 +1,4 @@
+
 // ALL INCOME/EXPENSE CARDS GO HERE
 let cardSelector = document.querySelector(".examples");
 
@@ -11,7 +12,15 @@ let backEnd = document.querySelector(".send");
 // MODAL
 let editModal = document.querySelector("#popup1");
 
-console.log(backEnd)
+// console.log(backEnd)
+
+
+
+// TOGGLE class to show and hide  all examples
+viewSelector.addEventListener("click", () => {
+    cardSelector.classList.toggle("hide");
+})
+
 
 
 
@@ -29,7 +38,43 @@ fetch("http://localhost:3000/incomes")
 
     }
     updateCard();
+
+    let calcButtons = document.querySelectorAll(".calculate");
+
+    for (const calcButton of calcButtons) {
+        // console.log(calcButton)
+        calcButton.addEventListener("click", (evt) => {
+            let calcBox = evt.target.parentElement.parentElement.parentElement;
+            // console.log(calcBox);
+
+            let dailyFoodAmount = calcBox.querySelector(".fAmount");
+            let monthlyTravelAmount = calcBox.querySelector(".tAmount");
+            let monthlyOtherAmount = calcBox.querySelector(".othAmount");
+            // console.log(dailyFoodAmount)
+        
+            
+            let foodNumberInside = parseInt(dailyFoodAmount.innerText.slice(9));
+            let travelNumberInside = parseInt(monthlyTravelAmount.innerText.slice(9));
+            let otherNumberInside = parseInt(monthlyOtherAmount.innerText.slice(9));
+            // console.log(numberInside)
+
+            let convertedFoodExpenses = dailyFood(foodNumberInside);
+            let convertedTravelExpenses = monthlyTravel(travelNumberInside);
+            let convertedOtherExpenses = monthlyOther(otherNumberInside);
+            console.log(convertedFoodExpenses)
+            console.log(convertedTravelExpenses)
+            console.log(convertedOtherExpenses)
+            
+        })
+    }
+
+
+
+
 })
+
+
+
 
 
 
@@ -38,7 +83,9 @@ fetch("http://localhost:3000/incomes")
 backEnd.addEventListener("submit", (evt) => {
     evt.preventDefault();
     console.log(evt.target.parentElement)
-    debugger
+
+    let selectedForm = evt.target.parentElement;
+    // debugger
 
     // INCOME
     let name = document.querySelector("#name");
@@ -85,27 +132,24 @@ backEnd.addEventListener("submit", (evt) => {
     .then(r => r.json())
     .then(res => {
         console.log(res)
+        
+         // make another card
+         cardMaker(res);
+         // make another edit event listener for newly made card
+         updateCard();
+        // debugger
 
-        // make another card
-        cardMaker(res);
-        // make another edit event listener for newly made card
-        updateCard();
+        let refreshForm  = selectedForm.firstElementChild.firstElementChild
 
+        emptyForm(refreshForm)
 
+        console.log(backEnd)
 
     })
 
-
 })
 
 
-
-
-
-// TOGGLE class to show and hide  all examples
-viewSelector.addEventListener("click", () => {
-    cardSelector.classList.toggle("hide");
-})
 
 
 
@@ -124,14 +168,14 @@ let updateCard = () => {
             let id = evt.target.parentElement.parentElement.parentElement.dataset.id;
             let selectedBox = evt.target.parentElement.parentElement.parentElement
 
-            console.log(selectedBox)
+            // console.log(selectedBox)
 
             editModal.classList.add("overlay")
 
             fetch(`http://localhost:3000/incomes/${id}`)
             .then(r => r.json())
             .then(res => {
-                console.log(res)
+                // console.log(res)
 
                 // make Modal form
                 createEditForm(res);
@@ -207,25 +251,24 @@ let updateCard = () => {
                                         <h6>Weekly: $ ${incomeObject.weekly}</h6>
                                         <h6>Daily: $ ${incomeObject.daily}</h6>
                                     </div>
-
                                     <div class="col expenseSection">
                                         <br>
                                         <h5>EXPENSE </h5>
                                         <br>
-                                        <h6>Category: ${allExpenses[0].category}</h6>
-                                        <h6>Amount: ${allExpenses[0].amount}</h6>
+                                        <h6 class="food">Category: ${allExpenses[0].category}</h6>
+                                        <h6 class="fAmount">Amount: ${allExpenses[0].amount}</h6>
                                         <br>
-                                        <h6>Category: ${allExpenses[1].category}</h6>
-                                        <h6>Amount: ${allExpenses[1].amount}</h6>
+                                        <h6 class="travel">Category: ${allExpenses[1].category}</h6>
+                                        <h6 class="tAmount">Amount: ${allExpenses[1].amount}</h6>
                                         <br>
-                                        <h6>Category: ${allExpenses[2].category}</h6>
-                                        <h6>Amount: ${allExpenses[2].amount}</h6>
+                                        <h6 class="other">Category: ${allExpenses[2].category}</h6>
+                                        <h6 class="othAmount">Amount: ${allExpenses[2].amount}</h6>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col">
                                         <a class="button" href="#popup1">Edit</a>
+                                        <button type="button" class="btn btn-outline-success calculate">Calculate</button>
                                     </div>
                                 </div> 
                         `;
@@ -252,11 +295,64 @@ let updateCard = () => {
 
 
 
-//FUNCTION to make INCOME/EXPENSE card
+
+
+
+
+//************************** */ INNER HTML'S *****************************************
+
+
+
+// EMPTY FORM
+let emptyForm =  (form) => {
+    form.innerHTML = `
+        <div class="row">
+            <div class="col">
+            <h3>Yearly Income</h3>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter Name">
+            </div>
+            <div class="form-group">
+                <label for="yearlySalary">How Much do you make a Year?</label>
+                <input type="number" class="form-control yrSalary" id="yearlySalary" placeholder="Yearly Salary" value="">
+                <small id="emailHelp" class="form-text text-muted">We will Transform this into Monthly, Weekly, and Daily Cash you have Available</small>
+            </div> 
+            </div>
+
+            <div class="col">
+            <h3>Expenses</h3>
+            <div class="form-group">
+                <label for="foodExpense">How much do you spend on <strong>FOOD</strong> in a <strong>DAILY</strong> Basis</label>
+                <input type="number" class="form-control foodExpense" id="foodExpense" placeholder="Enter Amount">
+            </div>
+            <div class="form-group">
+                <label for="travelExpense">How much do you spend on <strong>TRAVEL</strong> in a <strong>MONTHLY</strong> Basis</label>
+                <input type="number" class="form-control travelExpense" id="travelExpense" placeholder="Enter Amount">
+            </div>
+            <div class="form-group">
+                <label for="other">What are your <strong>OTHER EXPENSES</strong> in a <strong>MONTHLY</strong> Basis</label>
+                <input type="number" class="form-control otherExpense" id="other" placeholder="Enter Amount">
+                <small id="emailHelp" class="form-text text-muted">Other Expenses include total amount for credit card payments, Insurance, Rent, etc.</small>
+            </div> 
+            </div>
+        </div>  
+    `;
+
+}
+
+
+
+
+
+
+
+
+// Make INCOME/EXPENSE card HTML/JSON Response
 let cardMaker = (jsonRes) => {
     cardSelector.innerHTML +=
      `
-        <div class="card card-1" data-id="${jsonRes.id}">
+        <div class="card card-1 animated flipInX" data-id="${jsonRes.id}">
             <div class="row">
                 <div class="col incomeSection">
                     <br>
@@ -268,29 +364,30 @@ let cardMaker = (jsonRes) => {
                     <h6>Weekly: $ ${jsonRes.weekly}</h6>
                     <h6>Daily: $ ${jsonRes.daily}</h6>
                 </div>
-
                 <div class="col expenseSection">
                     <br>
                     <h5>EXPENSE </h5>
                     <br>
-                    <h6>Category: ${jsonRes.expenses[0].category}</h6>
-                    <h6>Amount: ${jsonRes.expenses[0].amount}</h6>
+                    <h6 class="food">Category: ${jsonRes.expenses[0].category}</h6>
+                    <h6 class="fAmount">Amount: $${jsonRes.expenses[0].amount}</h6>
                     <br>
-                    <h6>Category: ${jsonRes.expenses[1].category}</h6>
-                    <h6>Amount: ${jsonRes.expenses[1].amount}</h6>
+                    <h6 class="travel">Category: ${jsonRes.expenses[1].category}</h6>
+                    <h6 class="tAmount">Amount: $${jsonRes.expenses[1].amount}</h6>
                     <br>
-                    <h6>Category: ${jsonRes.expenses[2].category}</h6>
-                    <h6>Amount: ${jsonRes.expenses[2].amount}</h6>
+                    <h6 class="other">Category: ${jsonRes.expenses[2].category}</h6>
+                    <h6 class="othAmount">Amount: $${jsonRes.expenses[2].amount}</h6>
                 </div>
             </div>
-
-            <div class="row">
+            <div class="btn-group">
                 <div class="col">
                     <a class="button" href="#popup1">Edit</a>
+                    <button type="button" class="btn btn-outline-success calculate">Calculate</button>
                 </div>
             </div>
         </div>
         `;
+
+
 
 }
 
@@ -302,15 +399,14 @@ let cardMaker = (jsonRes) => {
 
 
 
-// FUNCTION to make Edit modal
+// Edit modal HTML
 let createEditForm = (jsonRes) => {
     editModal.innerHTML = `
-    <div class="popup">
+    <div class="popup animated rubberBand">
         <h2>EDIT</h2>
         <a class="close" href="#">&times;</a>
         <div class="content">
             <div class="container">
-
                 <form class="sendEdit">
             
                     <div class="row">
@@ -359,6 +455,19 @@ let createEditForm = (jsonRes) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ********************** CALCULATIONS *************************************
 
 // convert YEARLY SALARY 
@@ -386,11 +495,62 @@ let calculate = (yearly, name) => {
 
 // convert DAILY FOOD EXPENSE
 let dailyFood = (dailyAmount) => {
-    let yearly = Math.round(dailyAmount * 365);
-    let monthly = Math.round(yearly / 12);
+    let dAmount = dailyAmount
+    let yearly = Math.floor(dAmount * 365);
+    let monthly = Math.floor(yearly / 12);
+    let weekly = Math.floor(yearly / 52);
+
+
+    // console.log(yearly, monthly, weekly, dAmount)
+
+    dailyAmountObj = {
+        daily: dAmount,
+        yearlyAmount: yearly,
+        monthlyAmount: monthly,
+        weeklyAmount: weekly
+    }
+
+    return dailyAmountObj;
+}
+
+
+// convert MONTHLY TRAVEL EXPENSE
+let monthlyTravel = (monthlyAmount) => {
+    let mAmount = monthlyAmount;
+    let yearly = Math.round(mAmount * 12);
     let weekly = Math.round(yearly / 52);
+    let daily = Math.round(yearly / 365);
+
+    // console.log(yearly, weekly, daily, mAmount)
+
+    monthlyAmountObj = {
+        monthly: mAmount,
+        yearlyAmount: yearly,
+        weeklyAmount: weekly,
+        dailyAmount: daily
+    }
+
+    return monthlyAmountObj;
+}
 
 
-    console.log(yearly, monthly, weekly)
+
+// convert MONTHLY OTHER EXPENSE
+let monthlyOther = (monthlyOther) => {
+    let mOtherAmount = monthlyOther;
+    let yearly = Math.round(mOtherAmount * 12);
+    let weekly = Math.round(yearly / 52);
+    let daily = Math.round(yearly / 365);
+
+    // console.log(yearly, weekly, daily, mAmount)
+
+    monthlyOtherAmountObj = {
+        monthly: mOtherAmount,
+        yearlyAmount: yearly,
+        weeklyAmount: weekly,
+        dailyAmount: daily
+    }
+
+    return monthlyOtherAmountObj;
 }
 
